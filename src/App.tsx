4ccb9b0fba362bf7cc5c9971cffabbd29be6f248
aleixanadon <aleixanadon25@gmail.com>
2025-05-26 21:09:09 +0200

    update: imports of images

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import BlogPreviewCard from "./components/BlogPreviewCard/BlogPreviewCard";
 import type { BlogPreviewCardProps } from "./interfaces/BlogPreviewCardProps";
+import MainImage from "./assets/main_image.png"; 
+import AuthorImage from "./assets/userpic.png"
 
 function App() {
 
   const blogPreviewCardMock: BlogPreviewCardProps = {
-    mainImage: "src/assets/main_image.png",
+    mainImage: MainImage,
     pillText: "Learning",
     publishedDate: "21 Dec 2023",
     title: "HTML & CSS foundations",
     paragraph: "These languages are the backbone of every website, defining structure, content, and presentation.",
-    authorImage: "src/assets/userpic.png",
+    authorImage: AuthorImage,
     author: "Greg Hooper"
   }
 
